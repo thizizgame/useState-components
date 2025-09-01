02155df22d422e5faece3b20836d7b5fc63b2d0e
thizizgame <keepreshkaaa@gmail.com>
2025-09-01 21:25:09 +0800

    haha

--- src/app/multi_steps_form/page.js
+++ src/app/multi_steps_form/page.js
@@ -11,73 +11,58 @@ const multiStepsForm = () => {
     phone: "",
     passport: "",
     confirm: "",
+    birthday: "",
+    image: "",
   });
-  const submit = () =>{
-    // setForm({fName: form.fName, lName: form.lName , uName: form.uName}); 
-    setStep("step2");
-    console.log(form);
-  }
-  const back = () =>{
-    setStep("step1");
-  }
-  const backStep = () => {
-    setStep("step2");
-  }
-  const nextSubmit = () => {
-    setStep("step3");
-    console.log(form);
-  }
-  const allDone = () => {
-    setStep("step4");
-  }
+
   if(step === "step1")
   {
     return (
-    <div className="m-0 p-0 bg-white h-screen text-black box-content pt-[140px]">
-      <div className="flex flex-col gap-3 w-[500px] m-auto border-4 p-[32px]">
+    <div className="m-0 p-0 bg-[#F4F4F4] h-screen text-black box-content pt-[140px] text-[14px]">
+      <div className="flex flex-col gap-3 w-[500px] m-auto rounded-2xl shadow-sm p-[32px] bg-white">
         <img src="plogo.png" className="w-[60px]"/>
         <h1 className="text-[26px] font-semibold">Join Us! 😎</h1> 
         <p className="text-[#8E8E8E]">Please provide all current information accurately.</p>
-        First name * <input className="border-1 h-[44px] rounded-[5px] pl-[13px]" placeholder="Placeholder" 
+        First name * <input className="border-1 border-[#CBD5E1] h-[44px] rounded-[5px] pl-[13px] focus:outline-none focus:ring-0 focus:border-[#0CA5E9]" placeholder="Placeholder" 
         onChange={(e)=> setForm({
             ...form, full:e.target.value,
         })} value={form.full} ></input>
-        Last name *<input className="border-1 h-[44px] rounded-[5px] pl-[13px]" placeholder="Placeholder" value={form.last} 
+        Last name *<input className="border-1 border-[#CBD5E1] h-[44px] rounded-[5px] pl-[13px] focus:outline-none focus:ring-0 focus:border-[#0CA5E9]" placeholder="Placeholder" value={form.last} 
         onChange={(e)=> setForm({
             ...form, last:e.target.value,
         })}></input>
-        Username *<input className="border-1 h-[44px] rounded-[5px] pl-[13px]" placeholder="Placeholder" value={form.user} 
+        Username *<input className="border-1 border-[#CBD5E1] h-[44px] rounded-[5px] pl-[13px] focus:outline-none focus:ring-0 focus:border-[#0CA5E9]" placeholder="Placeholder" value={form.user} 
         onChange={(e)=> setForm({
             ...form, user:e.target.value,
         })}></input>
-       <button className="mt-[162px] bg-black text-white h-[44px] rounded-[5px]" onClick={submit}>Continue 1/3</button>
+       <button className="mt-[162px] bg-black text-white h-[44px] rounded-[5px]" onClick={()=>setStep("step2")}>Continue 1/3</button>
       </div>
     </div>
   );
   }
   if(step === "step2"){
     return (
-    <div className="m-0 p-0 bg-white h-screen text-black box-content pt-[140px]">
-      <div className="flex flex-col gap-3 w-[500px] m-auto border-4 p-[32px]">
+    <div className="m-0 p-0 bg-[#F4F4F4] h-screen text-black box-content pt-[140px] text-[14px]">
+      <div className="flex flex-col gap-3 w-[500px] m-auto rounded-2xl shadow-sm p-[32px] bg-white">
         <img src="plogo.png" className="w-[60px]"/>
         <h1 className="text-[26px] font-semibold">Join Us! 😎</h1> 
         <p className="text-[#8E8E8E]">Please provide all current information accurately.</p>
-        Email *<input className="border-1 h-[44px] rounded-[5px] pl-[13px]" placeholder="Placeholder" value={form.email} 
+        Email *<input className="border-1 border-[#CBD5E1] h-[44px] rounded-[5px] pl-[13px] focus:outline-none focus:ring-0 focus:border-[#0CA5E9]" placeholder="Placeholder" value={form.email} 
         onChange={(e)=> setForm({
             ...form, email:e.target.value,
         })}></input>
-        Phone number *<input className="border-1 h-[44px] rounded-[5px] pl-[13px]" placeholder="Placeholder" value={form.phone} onChange={(e)=> setForm({
+        Phone number *<input className="border-1 border-[#CBD5E1] h-[44px] rounded-[5px] pl-[13px] focus:outline-none focus:ring-0 focus:border-[#0CA5E9]" placeholder="Placeholder" value={form.phone} onChange={(e)=> setForm({
             ...form, phone:e.target.value,
         })}></input>
-        Password *<input className="border-1 h-[44px] rounded-[5px] pl-[13px]" type="password" placeholder="Placeholder" value={form.passport} onChange={(e)=> setForm({
+        Password *<input className="border-1 border-[#CBD5E1] h-[44px] rounded-[5px] pl-[13px] focus:outline-none focus:ring-0 focus:border-[#0CA5E9]" type="password" placeholder="Placeholder" value={form.passport} onChange={(e)=> setForm({
             ...form, passport:e.target.value,
         })}></input>
-        Confirm *<input className="border-1 h-[44px] rounded-[5px] pl-[13px]" type="password" placeholder="Placeholder" value={form.confirm} onChange={(e)=> setForm({
+        Confirm *<input className="border-1 border-[#CBD5E1] h-[44px] rounded-[5px] pl-[13px] focus:outline-none focus:ring-0 focus:border-[#0CA5E9]" type="password" placeholder="Placeholder" value={form.confirm} onChange={(e)=> setForm({
             ...form, confirm:e.target.value,
         })}></input>
         <div className="flex gap-5">
-        <button className="mt-[70px] bg-white text-black h-[44px] rounded-[5px] border-1 w-[128px]" onClick={back}>Back</button>
-        <button className="mt-[70px] bg-black text-white h-[44px] rounded-[5px] w-[280px]" onClick={nextSubmit}>Continue 2/3</button>
+        <button className="mt-[70px] bg-white text-black h-[44px] rounded-[5px] border-1 border-[#CBD5E1] w-[128px]" onClick={()=>setStep("step1")}>Back</button>
+        <button className="mt-[70px] bg-black text-white h-[44px] rounded-[5px] w-[280px]" onClick={()=>setStep("step3")}>Continue 2/3</button>
         </div>
        
       </div>
@@ -87,18 +72,18 @@ const multiStepsForm = () => {
 
   if(step=== "step3"){
     return (
-    <div className="m-0 p-0 bg-white h-screen text-black box-content pt-[140px]">
-      <div className="flex flex-col gap-3 w-[500px] m-auto border-4 p-[32px]">
+    <div className="m-0 p-0 bg-[#F4F4F4] h-screen text-black box-content pt-[140px] text-[14px]">
+      <div className="flex flex-col gap-3 w-[500px] m-auto rounded-2xl shadow-sm p-[32px] bg-white">
         <img src="plogo.png" className="w-[60px]"/>
         <h1 className="text-[26px] font-semibold">Join Us! 😎</h1> 
         <p className="text-[#8E8E8E]">Please provide all current information accurately.</p>
         Date of birth *
-        <input type="date"></input>
+        <input type="date" value={form.birthday}></input>
         Profile image *
         
        <div className="flex gap-5">
-        <button className="mt-[70px] bg-white text-black h-[44px] rounded-[5px] border-1 w-[128px]" onClick={backStep}>Back</button>
-        <button className="mt-[70px] bg-black text-white h-[44px] rounded-[5px] w-[280px]" onClick={allDone}>Continue 3/3</button>
+        <button className="mt-[70px] bg-white text-black h-[44px] rounded-[5px] border-1 border-[#CBD5E1] w-[128px]" onClick={()=>setStep("step2")}>Back</button>
+        <button className="mt-[70px] bg-black text-white h-[44px] rounded-[5px] w-[280px]" onClick={()=>setStep("step4")}>Continue 3/3</button>
         </div>
       </div>
     </div>
@@ -106,8 +91,8 @@ const multiStepsForm = () => {
   }
   if(step === "step4"){
     return (
-        <div className="m-0 p-0 bg-white h-screen text-black box-content pt-[140px]">
-      <div className="flex flex-col gap-3 w-[500px] m-auto border-4 p-[32px]">
+        <div className="m-0 p-0 bg-[#F4F4F4] h-screen text-black box-content pt-[140px] text-[14px]">
+      <div className="flex flex-col gap-3 w-[500px] m-auto rounded-2xl shadow-sm p-[32px] bg-white">
         <img src="plogo.png" className="w-[60px]"/>
         <h1 className="text-[26px] font-semibold">You're All Set</h1> 
         <p className="text-[#8E8E8E]">We have received your submission. Thank you!</p>
